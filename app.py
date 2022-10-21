6d01f2fd7c4b17b9d6663b405493800e59ab55dd
ManojKumarBV <58429776+ManojKumarBV@users.noreply.github.com>
2022-10-22 01:37:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ swaggerui_blueprint = get_swaggerui_blueprint(
 app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 if __name__ == "__main__":
-    app.run(host='text-reader-api.netlify.app')
+    app.run()
